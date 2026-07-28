ff9448c9950885ae206169b1ce8e0b3de78748c6
FlawedApostle <smafearnley@gmail.com>
2026-07-28 14:30:27 -0400

    Initial Unreal project commit

--- Source/Liminal_1.Target.cs
+++ Source/Liminal_1.Target.cs
@@ -8,8 +8,8 @@ public class Liminal_1Target : TargetRules
 	public Liminal_1Target(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V6;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
+		DefaultBuildSettings = BuildSettingsVersion.V7;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_8;
 		ExtraModuleNames.Add("Liminal_1");
 	}
 }
--- Source/Liminal_1Editor.Target.cs
+++ Source/Liminal_1Editor.Target.cs
@@ -8,8 +8,8 @@ public class Liminal_1EditorTarget : TargetRules
 	public Liminal_1EditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V6;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
+		DefaultBuildSettings = BuildSettingsVersion.V7;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_8;
 		ExtraModuleNames.Add("Liminal_1");
 	}
 }
